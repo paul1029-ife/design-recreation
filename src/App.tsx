1a56be79ca1448e2b836e4e8c93d1874a8c5aaa5
TheOne001 <paul.agbogun@gmail.com>
2025-10-11 22:14:20 +0100

    feat: Remove vercel analytics(for now)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import InviteCard from "./components/recreations/InviteCard";
 import type { PlaygroundComponent } from "./types";
 import Modal from "./components/global/Modal";
 import ComponentCard from "./components/global/ComponentCard";
-import { Analytics } from "@vercel/analytics/next";
 
 const components: PlaygroundComponent[] = [
   {
@@ -58,7 +57,6 @@ function App() {
         component={selectedComponent?.component}
         name={selectedComponent?.name}
       />
-      <Analytics />
     </div>
   );
 }
