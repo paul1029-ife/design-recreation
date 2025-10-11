372a9b6f3b7826f3da88a60e71c7093b0e3815bd
TheOne001 <paul.agbogun@gmail.com>
2025-10-11 21:26:47 +0100

    feat: Add styles for playgwound components.

---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+import { type ComponentType } from "react";
+
+export interface PlaygroundComponent {
+  id: number;
+  name: string;
+  source: string;
+  component: ComponentType;
+}
+
+export interface ComponentCardProps {
+  name: string;
+  source: string;
+  onClick: () => void;
+}
+
+export interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  component?: ComponentType;
+  name?: string;
+}
