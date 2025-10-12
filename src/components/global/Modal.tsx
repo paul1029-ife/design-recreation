f28a8fcbea8f48127703a757e5fbc1171fd4b563
TheOne001 <paul.agbogun@gmail.com>
2025-10-12 17:11:09 +0100

    feat: Remove background from component and use a simple overlay

--- src/components/global/Modal.tsx
+++ src/components/global/Modal.tsx
@@ -1,8 +1,7 @@
-import { X } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
 import type { ModalProps } from "../../types";
 
-const Modal = ({ isOpen, onClose, component: Component, name }: ModalProps) => {
+const Modal = ({ isOpen, onClose, component: Component }: ModalProps) => {
   return (
     <AnimatePresence>
       {isOpen && (
@@ -25,18 +24,8 @@ const Modal = ({ isOpen, onClose, component: Component, name }: ModalProps) => {
             animate={{ opacity: 1, scale: 1, y: 0 }}
             exit={{ opacity: 0, scale: 0.95, y: 20 }}
             transition={{ duration: 0.25, ease: "easeOut" }}
-            className="relative bg-white rounded-2xl h-3/4 shadow-xl max-w-4xl w-full mx-2 md:mx-4 overflow-scroll scrollbar-hide border border-gray-200"
+            className="relative border-none h-3/4 max-w-4xl  mx-2 md:mx-4 overflow-scroll scrollbar-hide border"
           >
-            <div className="flex items-center justify-between px-6 py-4 border-b border-gray-200 bg-white">
-              <h2 className="text-xl font-semibold text-black">{name}</h2>
-              <button
-                onClick={onClose}
-                className="text-gray-400 hover:text-black transition-colors ease-in duration-250"
-              >
-                <X size={22} />
-              </button>
-            </div>
-
             {/* Component Display */}
             <div className="p-8 flex items-center justify-center min-h-[400px]">
               {Component && <Component />}
