fde878114599c69aec93009b7692f13ea4ae4b8c
TheOne001 <paul.agbogun@gmail.com>
2025-10-28 01:59:03 +0100

    fix: Add proper width measurement and damping to animation.

--- src/components/recreations/InlineOverflow.tsx
+++ src/components/recreations/InlineOverflow.tsx
@@ -1,81 +1,129 @@
-import { MoreHorizontal, X } from "lucide-react";
-import { AnimatePresence, motion } from "motion/react";
+import { Ellipsis, X, Save, Copy, Share, Trash2 } from "lucide-react";
+import {
+  AnimatePresence,
+  motion,
+  MotionConfig,
+  type Transition,
+} from "motion/react";
 import { useState } from "react";
+import useMeasure from "react-use-measure";
 
 const InlineOverflow = () => {
-  const [isFull, setIsFull] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [ref, bounds] = useMeasure({ offsetSize: false });
+  const SPRING_TRANSITION: Transition = {
+    type: "spring",
 
-  const handleClick = () => {
-    setIsFull(!isFull);
+    stiffness: 200,
+    damping: 18,
   };
 
   return (
     <div
-      className="flex flex-col items-center justify-center p-4 gap-8 "
-      style={{ fontFamily: "Some-Sans" }}
+      className="flex flex-col items-center justify-center p-4 gap-8"
+      style={{ fontFamily: "system-ui, -apple-system, sans-serif" }}
     >
-      <motion.div className="rounded-full bg-stone-100 flex gap-1.5 px-2 py-1.5 items-center">
-        <div className="px-3 py-2 rounded-full bg-white">Save</div>
-        <div className="px-3 py-2 rounded-full bg-white">Copy</div>
-        <AnimatePresence mode="wait">
-          {isFull && (
-            <motion.div
-              className="flex gap-1.5 overflow-hidden"
-              initial={{ width: 0 }}
-              animate={{ width: "auto" }}
-              exit={{
-                width: 0,
-                marginRight: -12,
-                transition: {
-                  type: "spring",
-                  bounce: 0.1,
-                  duration: 0.37,
-                },
-              }}
-              transition={{
-                type: "spring",
-                bounce: 0.35,
-                duration: 0.8,
-              }}
+      <MotionConfig transition={SPRING_TRANSITION}>
+        <motion.div
+          animate={{
+            width: isOpen ? bounds.width : 210,
+          }}
+          className="bg-stone-500 rounded-full shadow-lg"
+          transition={{
+            type: "spring",
+            stiffness: 200,
+            damping: 14,
+          }}
+        >
+          <div className="flex items-center justify-center overflow-hidden">
+            <div
+              className=" gap-1.5 flex items-center text-sm font-semibold p-1.5 "
+              ref={ref}
             >
               <motion.div
-                className="px-3 py-2 rounded-full bg-white whitespace-nowrap"
-                initial={{ opacity: 0 }}
-                animate={{
-                  opacity: 1,
-                  transition: {
-                    delay: 0.16,
-                    duration: 0.2,
-                  },
-                }}
-                exit={{ opacity: 0, transition: { duration: 0.15 } }}
+                className="flex items-center justify-center gap-1.5 "
+                layout="position"
               >
-                Share
+                <div className="bg-white rounded-4xl px-3 py-2 flex items-center justify-center gap-1.5 z-10 shadow-sm">
+                  <Save className="size-3.5" />
+                  Save
+                </div>
+                <div className="bg-white rounded-4xl px-3 py-2 flex items-center justify-center gap-1.5 z-10 shadow-sm">
+                  <Copy className="size-3.5" />
+                  Copy
+                </div>
               </motion.div>
+
+              <AnimatePresence mode="popLayout">
+                {isOpen && (
+                  <motion.div
+                    className="flex items-center gap-1.5 "
+                    initial={{
+                      opacity: 0,
+                      filter: "blur(4px)",
+                    }}
+                    animate={{
+                      opacity: 1,
+                      scale: 1,
+                      x: 0,
+                      animationDelay: 10,
+                      filter: "blur(0px)",
+                    }}
+                    exit={{
+                      opacity: 0,
+                      filter: "blur(4px)",
+                      z: -10,
+                    }}
+                    transition={{
+                      duration: 0.15,
+                      ease: "easeOut",
+                    }}
+                  >
+                    <div className="bg-white rounded-4xl px-3 py-2 flex items-center justify-center gap-1.5 shadow-sm">
+                      <Share className="size-3.5" />
+                      Share
+                    </div>
+                    <div className="bg-white rounded-4xl px-3 py-2 flex items-center justify-center gap-1.5 shadow-sm">
+                      <Trash2 className="size-3.5" />
+                      Delete
+                    </div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+
               <motion.div
-                className="px-3 py-2 rounded-full bg-white whitespace-nowrap"
-                initial={{ opacity: 0 }}
-                animate={{
-                  opacity: 1,
-                  transition: {
-                    delay: 0.16,
-                    duration: 0.2,
-                  },
-                }}
-                exit={{ opacity: 0, transition: { duration: 0.15 } }}
+                className="bg-white rounded-4xl size-7 flex items-center justify-center cursor-pointer z-10 shadow-sm"
+                onClick={() => setIsOpen(!isOpen)}
+                layout="position"
               >
-                Delete
+                <AnimatePresence mode="popLayout" initial={false}>
+                  <motion.div
+                    key={isOpen ? "x" : "elipsis"}
+                    initial={{
+                      opacity: 0,
+                      scale: 0.25,
+                      filter: "blur(4px)",
+                    }}
+                    animate={{ opacity: 1, scale: 1, filter: "blur(0px)" }}
+                    exit={{ opacity: 0, scale: 0.25, filter: "blur(4px)" }}
+                    transition={{
+                      type: "spring",
+                      duration: 0.4,
+                      bounce: 0,
+                    }}
+                  >
+                    {!isOpen ? (
+                      <Ellipsis className="text-neutral-900 size-4" />
+                    ) : (
+                      <X className="text-neutral-500 size-4" />
+                    )}
+                  </motion.div>
+                </AnimatePresence>
               </motion.div>
-            </motion.div>
-          )}
-        </AnimatePresence>
-        <button
-          className="bg-white rounded-full size-8 flex items-center justify-center hover:text-gray-500 text-gray-900 transition-colors"
-          onClick={handleClick}
-        >
-          {isFull ? <X size={20} /> : <MoreHorizontal size={20} />}
-        </button>
-      </motion.div>
+            </div>
+          </div>
+        </motion.div>
+      </MotionConfig>
     </div>
   );
 };
