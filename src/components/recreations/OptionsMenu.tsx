3f7cdac93ae0d1691cf678aacdd1777d86adcb06
TheOne001 <paul.agbogun@gmail.com>
2026-01-06 18:12:34 +0100

    fix: add small delay to fix plus icon expansion.

--- src/components/recreations/OptionsMenu.tsx
+++ src/components/recreations/OptionsMenu.tsx
@@ -41,7 +41,12 @@ export default function OptionsMenu() {
               borderRadius: 20,
             }}
           >
-            <Plus size={18} />
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, transition: { delay: 0.25 } }}
+            >
+              <Plus size={18} />
+            </motion.span>
 
             <motion.span
               layoutId="create-text"
